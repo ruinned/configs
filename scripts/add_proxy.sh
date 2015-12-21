f217c591c7283ceabefab305bfbf188da8061035
Yilin.Sun <yilin.sun@desay-svautomotive.com>
2015-12-21 18:18:32 +0800

    update proxy address.

--- scripts/add_proxy.sh
+++ scripts/add_proxy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export http_proxy=http://10.0.0.206:3128/
-export ftp_proxy=http://10.0.0.206:3128/
-export all_proxy=socks://10.0.0.206:3128/
-export https_proxy=http://10.0.0.206:3128/
+export http_proxy=http://10.219.127.206:3128/
+export ftp_proxy=http://10.219.127.206:3128/
+export all_proxy=socks://10.219.127.206:3128/
+export https_proxy=http://10.219.127.206:3128/
 export no_proxy=localhost,127.0.0.0/8
