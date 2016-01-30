d1053360859424573d4f8ecb98f827d3d43fbeba
Yilin.Sun <yilin.sun@desay-svautomotive.com>
2016-01-30 13:43:18 +0800

    add link

---
+++ scripts/add_link.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ln -s ${HOME}/configs/tools/git/config/gitconfig ${HOME}/.gitconfig
+ln -s ${HOME}/configs/tools/vim/config/vimrc ${HOME}/.vimrc
+ln -s ${HOME}/configs/tools/vim/plugins ${HOME}/.vim
