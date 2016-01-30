2c1b8c87d1ec473334cb132a8230b6c669ba8d48
Yilin.Sun <yilin.sun@desay-svautomotive.com>
2016-01-30 16:07:44 +0800

    smartgit

--- scripts/smartgit_tuning.sh
+++ scripts/smartgit_tuning.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "-Xmx1024m" >> ${HOME}/.smartgit/7/smartgit.properties
+
 echo "Changing watch limit..."
 sudo sh -c "echo \"fs.inotify.max_user_watches = 102400\" >> /etc/sysctl.conf"
 sudo sysctl -p
