68df96f34433ca995f196c23d7bc2a4330c88209
Sun Yilin (uida3867) <yilin.sun@desay-svautomotive.com>
2017-04-07 09:41:20 +0800

    fix env issue.

--- scripts/cp_repo.sh
+++ scripts/cp_repo.sh
@@ -3,4 +3,4 @@
 sudo sh -c "cp ${HOME}/configs/tools/repo /usr/bin -av"
 
 echo "" >> ${HOME}/.bashrc
-echo "REPO_URL=git@scm-gitlab:ti-glsdk/git-repo" >> ${HOME}/.bashrc
+echo "export REPO_URL=git@scm-gitlab:ti-glsdk/git-repo" >> ${HOME}/.bashrc
